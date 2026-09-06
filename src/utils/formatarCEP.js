328c4b012f9b3aa4d9eedde06f6ddbb861574efe
Hellen Martins <martinshellen235@gmail.com>
2026-09-06 11:14:12 -0300

    ajusta funcao formatarCep

--- src/utils/formatarCEP.js
+++ src/utils/formatarCEP.js
@@ -1,8 +1,7 @@
-const formatarCEP = (cep) => {
-  if (!cep)
-    return "";
+export function formatarCEP(cep) {
+  if (!cep) return "";
 
   const apenasNumeros = cep.replace(/\D/g, "");
 
   return apenasNumeros.replace(/^(\d{5})(\d{3})$/, "$1-$2");
-};
+}
